e78a0203cd338c8677317fa37a7a6b61fad474a1
tack0sim <167250747+tack0sim@users.noreply.github.com>
2024-09-08 12:24:01 +0200

    rudimentary visual structure

--- calculator app/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
-  const burgerMenu = document.querySelector(".burger-menu");
-
-  burger.addEventListener("click", (event) => {
-    console.log(event);
-    burgerMenu.classList.toggle("active");
-  });
-});
